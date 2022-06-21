2164719e2b9745007339f89286065f15f378bf9c
juhyeonmin <lovewith0624@gmail.com>
2022-06-21 13:10:18 +0900

    upload note_representation.py

--- note_representation.py
+++ note_representation.py
@@ -2,12 +2,10 @@ import numpy as np
 import IPython.display as ipd
 import muspy
 
-BASE_DIR = './MIDI-BERT-CP/Data/CP_data/'
+song = np.load('predict.npy')
 
-data = np.load(BASE_DIR + 'pop909_train.npy')
-song = data[100]
 
-x = song.T
+x = song
 
 bar, sub_beat, pitch, dur = x[0], x[1], x[2], x[3]
 start_timestep = []
@@ -16,7 +14,9 @@ i=0
 for j in range(len(pitch)):
     if bar[j]==0:
         i += 1
-    start_timestep.append(84 * i + (sub_beat[j] - 1) * 12)
+    elif bar[j] ==2:
+      break
+    start_timestep.append(128 * i + (sub_beat[j] - 1) * 8)
 
 
 start_timestep = np.array(start_timestep)
@@ -26,17 +26,15 @@ velocity = np.array([64] * (j+1))
 #print(start_timestep.shape)
 #print(pitch.shape)
 
-note_repr = np.stack([start_timestep, pitch[:j+1], dur[:j+1]*12, velocity]).T
+note_repr = np.stack([start_timestep, pitch[:j+1]+24, dur[:j+1]*2, velocity]).T
 
 print(note_repr)
 
 ####
 muspy.download_bravura_font()
 muspy.download_musescore_soundfont()
+!sudo apt-get install fluidsynth
 gen_music = muspy.from_note_representation(note_repr)
 gen_audio = gen_music.synthesize().T
-ipd.Audio(gen_audio/2**10, rate=44100)
+ipd.Audio(gen_audio/2**15, rate=44100)
 
-import matplotlib as mpl
-mpl.rcParams['figure.figsize'] = [30., 20.]
-gen_music.show_score(font_scale=500)
